18cac232e845d0bcc49fb0ff6f92e7e17116acab
Austin Orth <aorth@niche.com>
2021-05-24 13:27:31 -0400

    Exercise 2 answers
    Signed-off-by: Austin Orth <aorth@niche.com>

--- module01/sum.go
+++ module01/sum.go
@@ -3,5 +3,16 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	sum := 0
+	for _, i := range numbers {
+		sum += i
+	}
+	return sum
+}
+
+func RecursiveSum(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+	return numbers[0] + RecursiveSum(numbers[1:])
 }
